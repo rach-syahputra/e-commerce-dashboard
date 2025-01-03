99506e593ff5d64c65291285e32d9ebfb39e13a8
rach-syahputra <rach.syahputra@gmail.com>
2025-01-03 13:45:49 +0800

    style: adjust Header component styles

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -4,7 +4,7 @@ type HeaderProps = {
 
 const Header = ({ children }: HeaderProps) => {
   return (
-    <h1 className='text-2xl font-bold uppercase text-zinc-800 dark:bg-gradient-to-b dark:from-gray-100 dark:to-gray-300 dark:bg-clip-text dark:text-transparent lg:text-3xl'>
+    <h1 className='text-xl font-bold uppercase text-zinc-800 dark:bg-gradient-to-b dark:from-gray-100 dark:to-gray-300 dark:bg-clip-text dark:text-transparent lg:text-2xl'>
       {children}
     </h1>
   )
