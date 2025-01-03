d736e646671788072900be695a4d0463d3f56e51
rach-syahputra <rach.syahputra@gmail.com>
2025-01-03 20:23:52 +0800

    style: adjust Paragraph component styles

--- src/components/paragraph/paragraph.tsx
+++ src/components/paragraph/paragraph.tsx
@@ -6,7 +6,11 @@ type ParagraphProps = {
 }
 
 const Paragraph = ({ children, className }: ParagraphProps) => {
-  return <p className={cn(className)}>{children}</p>
+  return (
+    <p className={cn('text-gray-900 dark:text-gray-100', className)}>
+      {children}
+    </p>
+  )
 }
 
 export default Paragraph
