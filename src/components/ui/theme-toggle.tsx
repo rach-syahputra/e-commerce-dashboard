046ce3d0ee7dcbc85176033782f3b9e970dc7b2c
rach-syahputra <rach.syahputra@gmail.com>
2025-01-03 13:45:35 +0800

    style: adjust ThemeToggle component styles

--- src/components/ui/theme-toggle.tsx
+++ src/components/ui/theme-toggle.tsx
@@ -13,6 +13,7 @@ export function ThemeToggle() {
       variant='outline'
       size='icon'
       onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+      className='rounded-full'
     >
       <Sun className='h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all duration-300 dark:-rotate-90 dark:scale-0' />
       <Moon className='absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
