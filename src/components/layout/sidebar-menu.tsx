af015b58461dee501a17ceedcba0f2a21a86c63c
rach-syahputra <rach.syahputra@gmail.com>
2025-01-04 17:02:02 +0800

    feat: add AppSidebarMenu component

---
+++ src/components/layout/sidebar-menu.tsx
@@ -0,0 +1,90 @@
+'use client'
+
+import Link from 'next/link'
+import {
+  Boxes,
+  ChartColumnDecreasing,
+  LayoutDashboard,
+  ShoppingCart,
+  SquareChartGantt,
+  StretchHorizontal,
+  UserRound
+} from 'lucide-react'
+
+import {
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem
+} from '../ui/sidebar'
+import { usePathname } from 'next/navigation'
+
+const managementItems = [
+  {
+    title: 'Dashboard',
+    url: '/',
+    icon: LayoutDashboard
+  },
+  {
+    title: 'Users',
+    url: '/users',
+    icon: UserRound
+  },
+  {
+    title: 'Products',
+    url: '/products',
+    icon: Boxes
+  },
+  {
+    title: 'Categories',
+    url: '/categories',
+    icon: StretchHorizontal
+  },
+  {
+    title: 'Orders',
+    url: '/orders',
+    icon: ShoppingCart
+  },
+  {
+    title: 'Analytics',
+    url: '/analytics',
+    icon: ChartColumnDecreasing
+  },
+  {
+    title: 'Reports',
+    url: '/reports',
+    icon: SquareChartGantt
+  }
+]
+
+const AppSidebarMenu = () => {
+  const pathname = usePathname()
+
+  return (
+    <SidebarGroup className='py-0'>
+      <SidebarGroupContent>
+        <SidebarGroupLabel>Management</SidebarGroupLabel>
+        <SidebarMenu>
+          {managementItems.map((item) => (
+            <SidebarMenuItem key={item.title}>
+              <SidebarMenuButton
+                asChild
+                className='py-6'
+                isActive={pathname === item.url}
+              >
+                <Link href={item.url} aria-label={`${item.title} page`}>
+                  <item.icon />
+                  <span>{item.title}</span>
+                </Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          ))}
+        </SidebarMenu>
+      </SidebarGroupContent>
+    </SidebarGroup>
+  )
+}
+
+export default AppSidebarMenu
--- src/components/layout/sidebar.tsx
+++ src/components/layout/sidebar.tsx
@@ -1,62 +1,5 @@
-import Link from 'next/link'
-import {
-  Boxes,
-  ChartColumnDecreasing,
-  LayoutDashboard,
-  ShoppingCart,
-  SquareChartGantt,
-  StretchHorizontal,
-  UserRound
-} from 'lucide-react'
-
-import {
-  Sidebar,
-  SidebarContent,
-  SidebarGroup,
-  SidebarGroupContent,
-  SidebarGroupLabel,
-  SidebarMenu,
-  SidebarMenuButton,
-  SidebarMenuItem
-} from '@/components/ui/sidebar'
-
-const managementItems = [
-  {
-    title: 'Dashboard',
-    url: '/',
-    icon: LayoutDashboard
-  },
-  {
-    title: 'Users',
-    url: '/users',
-    icon: UserRound
-  },
-  {
-    title: 'Products',
-    url: '/products',
-    icon: Boxes
-  },
-  {
-    title: 'Categories',
-    url: '/categories',
-    icon: StretchHorizontal
-  },
-  {
-    title: 'Orders',
-    url: '/orders',
-    icon: ShoppingCart
-  },
-  {
-    title: 'Analytics',
-    url: '/analytics',
-    icon: ChartColumnDecreasing
-  },
-  {
-    title: 'Reports',
-    url: '/reports',
-    icon: SquareChartGantt
-  }
-]
+import { Sidebar, SidebarContent } from '@/components/ui/sidebar'
+import AppSidebarMenu from './sidebar-menu'
 
 export function AppSidebar() {
   return (
@@ -65,23 +8,7 @@ export function AppSidebar() {
         <span className='flex h-14 select-none items-center px-4 text-xl font-bold'>
           E-COMMERCE
         </span>
-        <SidebarGroup className='py-0'>
-          <SidebarGroupContent>
-            <SidebarGroupLabel>Management</SidebarGroupLabel>
-            <SidebarMenu>
-              {managementItems.map((item) => (
-                <SidebarMenuItem key={item.title}>
-                  <SidebarMenuButton asChild className='py-6'>
-                    <Link href={item.url} aria-label={`${item.title} page`}>
-                      <item.icon />
-                      <span>{item.title}</span>
-                    </Link>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-              ))}
-            </SidebarMenu>
-          </SidebarGroupContent>
-        </SidebarGroup>
+        <AppSidebarMenu />
       </SidebarContent>
     </Sidebar>
   )
