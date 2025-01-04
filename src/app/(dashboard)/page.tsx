25b969fdb2fe5f8957f6ed9278342d0ac37cf36a
rach-syahputra <rach.syahputra@gmail.com>
2025-01-04 16:49:08 +0800

    feat: add Dashboard page

---
+++ src/app/(dashboard)/page.tsx
@@ -0,0 +1,10 @@
+import PageContainer from '@/components/container/page-container'
+import DashboardSummary from './_components/dashboard-summary'
+
+export default function Home() {
+  return (
+    <PageContainer>
+      <DashboardSummary />
+    </PageContainer>
+  )
+}
