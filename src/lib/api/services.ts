d4aded90efee2e3131edc26d902f3c1203910540
rach-syahputra <rach.syahputra@gmail.com>
2025-01-04 16:23:29 +0800

    refactor: remove cache config from fetch services

--- src/lib/api/services.ts
+++ src/lib/api/services.ts
@@ -1,33 +1,25 @@
 const BASE_URL = 'http://localhost:3004'
 
 export const fetchAllUser = async () => {
-  const res = await fetch(`${BASE_URL}/users`, {
-    cache: 'force-cache'
-  })
+  const res = await fetch(`${BASE_URL}/users`)
 
   return await res.json()
 }
 
 export const fetchAllProduct = async () => {
-  const res = await fetch(`${BASE_URL}/products`, {
-    cache: 'force-cache'
-  })
+  const res = await fetch(`${BASE_URL}/products`)
 
   return await res.json()
 }
 
 export const fetchAllCategory = async () => {
-  const res = await fetch(`${BASE_URL}/categories`, {
-    cache: 'force-cache'
-  })
+  const res = await fetch(`${BASE_URL}/categories`)
 
   return await res.json()
 }
 
 export const fetchAllOrder = async () => {
-  const res = await fetch(`${BASE_URL}/orders`, {
-    cache: 'force-cache'
-  })
+  const res = await fetch(`${BASE_URL}/orders`)
 
   return await res.json()
 }
