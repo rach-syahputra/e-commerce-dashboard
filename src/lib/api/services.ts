f0c19ba5527539c19e7966f6827580c26a1d9062
rach-syahputra <rach.syahputra@gmail.com>
2025-01-05 18:59:22 +0800

    feat: add fetchSingleData and fetchFilteredData services

--- src/lib/api/services.ts
+++ src/lib/api/services.ts
@@ -23,3 +23,33 @@ export const fetchAllOrder = async () => {
 
   return await res.json()
 }
+
+export const fetchSingleData = async (
+  data: 'users' | 'products' | 'orders' | 'categories',
+  id: string
+) => {
+  const res = await fetch(`${BASE_URL}/${data}?id=${id}`)
+
+  return await res.json()
+}
+
+export const fetchFilteredData = async (
+  data: 'users' | 'products' | 'orders' | 'categories',
+  filter?: {
+    sort?: string
+    page?: number
+    limit?: number
+    order?: string
+  }
+) => {
+  const params = new URLSearchParams()
+
+  if (filter?.sort) params.append('_sort', filter.sort)
+  if (filter?.page) params.append('_page', filter.page.toString())
+  if (filter?.limit) params.append('_limit', filter.limit.toString())
+  if (filter?.order) params.append('_order', filter.order)
+
+  const res = await fetch(`${BASE_URL}/${data}?${params.toString()}`)
+
+  return await res.json()
+}
