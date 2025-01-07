d1e0f2118cb3cea124ab55081aa3e80c2615387a
rach-syahputra <rach.syahputra@gmail.com>
2025-01-07 14:42:48 +0800

    feat: add Users Overview Page

---
+++ src/app/(dashboard)/(management)/users/overview/page.tsx
@@ -0,0 +1,14 @@
+import PageContainer from '@/components/container/page-container'
+import UsersChart from './_components/users-chart'
+import UsersSummary from './_components/users-summary'
+
+export default function UsersOverviewPage() {
+  return (
+    <PageContainer>
+      <div className='grid grid-cols-7 gap-4'>
+        <UsersChart className='order-2 col-span-7 lg:order-1 lg:col-span-5' />
+        <UsersSummary className='order-1 col-span-7 lg:order-2 lg:col-span-2' />
+      </div>
+    </PageContainer>
+  )
+}
