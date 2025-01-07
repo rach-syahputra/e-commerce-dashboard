4fd87bd7351401d0c60916ec4f4b6d991e60defd
rach-syahputra <rach.syahputra@gmail.com>
2025-01-07 14:55:23 +0800

    feat: add metadata configuration on DashboardLayout and UsersLayout

--- src/app/(dashboard)/(management)/layout.tsx
+++ src/app/(dashboard)/(management)/layout.tsx
@@ -1,4 +1,10 @@
+import { Metadata } from 'next'
 import ManagementNavbar from '@/components/layout/management-navbar'
+
+export const metadata: Metadata = {
+  title: 'User Management',
+  description: 'E-commerce User Management.'
+}
 
 export default function ManagementLayout({
   children
--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,9 +1,15 @@
+import { Metadata } from 'next'
 import { SessionProvider } from 'next-auth/react'
 
 import Container from '@/components/container/container'
 import { SidebarProvider } from '@/components/ui/sidebar'
 import { AppSidebar } from '@/components/layout/sidebar'
 import { AppNavbar } from '@/components/layout/navbar'
+
+export const metadata: Metadata = {
+  title: 'Dashboard',
+  description: 'E-commerce Dashboard.'
+}
 
 export default function DashboardLayout({
   children
