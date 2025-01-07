bef0f9e84dafa9cf4829beaf0a64cab4c3927faa
rach-syahputra <rach.syahputra@gmail.com>
2025-01-07 14:42:23 +0800

    feat: add UserDataTable component

---
+++ src/components/table/user/user-columns.tsx
@@ -0,0 +1,175 @@
+'use client'
+
+import Image from 'next/image'
+import { ArrowUpDown, MoreHorizontal } from 'lucide-react'
+import { ColumnDef } from '@tanstack/react-table'
+
+import { UserRoleType } from '@/lib/types/types'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger
+} from '../../ui/dropdown-menu'
+import { Button } from '../../ui/button'
+
+// This type is used to define the shape of our data.
+// You can use a Zod schema here if you want.
+export type UserTable = {
+  id: string
+  image: string
+  name: string
+  email: string
+  role: UserRoleType
+  createdAt: string
+  updatedAt: string
+}
+
+export const userColumns: ColumnDef<UserTable>[] = [
+  // ROW SELECTION
+  // {
+  //   id: 'select',
+  //   header: ({ table }) => (
+  //     <Checkbox
+  //       checked={
+  //         table.getIsAllPageRowsSelected() ||
+  //         (table.getIsSomePageRowsSelected() && 'indeterminate')
+  //       }
+  //       onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+  //       aria-label='Select all'
+  //     />
+  //   ),
+  //   cell: ({ row }) => (
+  //     <Checkbox
+  //       checked={row.getIsSelected()}
+  //       onCheckedChange={(value) => row.toggleSelected(!!value)}
+  //       aria-label='Select row'
+  //     />
+  //   ),
+  //   enableSorting: false,
+  //   enableHiding: false
+  // },
+  {
+    accessorKey: 'image',
+    header: 'Image',
+    maxSize: 40,
+    cell: ({ row }) => (
+      <Image
+        src={row.original.image}
+        alt='User image'
+        width={32}
+        height={32}
+        className='ml-1.5 h-6 w-6 rounded-full bg-gray-200 lg:h-8 lg:w-8'
+      />
+    )
+  },
+  {
+    accessorKey: 'name',
+    header: ({ column }) => {
+      return (
+        <Button
+          variant='table-header'
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+          className='px-0 text-xs lg:text-sm'
+        >
+          Name
+          <ArrowUpDown />
+        </Button>
+      )
+    }
+  },
+  {
+    accessorKey: 'email',
+    header: ({ column }) => {
+      return (
+        <Button
+          variant='table-header'
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+          className='px-0 text-xs lg:text-sm'
+        >
+          Email
+          <ArrowUpDown />
+        </Button>
+      )
+    }
+  },
+  {
+    accessorKey: 'role',
+    header: ({ column }) => {
+      return (
+        <Button
+          variant='table-header'
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+          className='px-0 text-xs lg:text-sm'
+        >
+          Role
+          <ArrowUpDown />
+        </Button>
+      )
+    }
+  },
+  {
+    accessorKey: 'createdAt',
+    header: ({ column }) => {
+      return (
+        <Button
+          variant='table-header'
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+          className='px-0 text-xs lg:text-sm'
+        >
+          CreatedAt
+          <ArrowUpDown />
+        </Button>
+      )
+    }
+  },
+  {
+    accessorKey: 'updatedAt',
+    header: ({ column }) => {
+      return (
+        <Button
+          variant='table-header'
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+          className='px-0 text-xs lg:text-sm'
+        >
+          UpdatedAt
+          <ArrowUpDown />
+        </Button>
+      )
+    }
+  },
+  {
+    id: 'actions',
+    cell: ({ row }) => {
+      const user = row.original
+
+      return (
+        <div className='flex w-full items-center justify-center'>
+          <DropdownMenu modal={false}>
+            <DropdownMenuTrigger className='border' asChild>
+              <Button variant='ghost' className='h-8 w-8 p-0'>
+                <span className='sr-only'>Open menu</span>
+                <MoreHorizontal className='h-4 w-4' />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align='end'>
+              <DropdownMenuLabel>Actions</DropdownMenuLabel>
+              <DropdownMenuItem
+                onClick={() => navigator.clipboard.writeText(user.id)}
+              >
+                Copy user ID
+              </DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>View detail</DropdownMenuItem>
+              <DropdownMenuItem className='text-destructive'>
+                Delete
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+      )
+    }
+  }
+]
---
+++ src/components/table/user/user-data-table.tsx
@@ -0,0 +1,37 @@
+import { formatDateForTable } from '@/lib/utils'
+import { IUserJson } from '@/lib/types/json'
+import { fetchAllUser } from '@/lib/api/services'
+import { DataTable } from '../data-table'
+import { userColumns, UserTable } from './user-columns'
+
+async function getData(): Promise<UserTable[]> {
+  const res: IUserJson[] = await fetchAllUser()
+
+  return res.map((user) => ({
+    ...user,
+    createdAt: formatDateForTable(new Date(user.createdAt)),
+    updatedAt: formatDateForTable(new Date(user.updatedAt))
+  }))
+}
+
+const UserDataTable = async () => {
+  const data = await getData()
+
+  const searchableColumns: string[] = [
+    'name',
+    'email',
+    'role',
+    'createdAt',
+    'updatedAt'
+  ]
+
+  return (
+    <DataTable
+      columns={userColumns}
+      data={data}
+      searchableColumns={searchableColumns}
+    />
+  )
+}
+
+export default UserDataTable
