1c7de86080a4be1d4369cc46dc35b7d105cae1d8
rach-syahputra <rach.syahputra@gmail.com>
2025-01-07 14:42:55 +0800

    feat: add Users Record Page

---
+++ src/app/(dashboard)/(management)/users/record/page.tsx
@@ -0,0 +1,10 @@
+import PageContainer from '@/components/container/page-container'
+import UserDataTable from '@/components/table/user/user-data-table'
+
+export default function UsersRecordPage() {
+  return (
+    <PageContainer>
+      <UserDataTable />
+    </PageContainer>
+  )
+}
