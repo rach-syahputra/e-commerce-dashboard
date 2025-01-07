08f34f08a493b30c73b1a7716479846483720640
rach-syahputra <rach.syahputra@gmail.com>
2025-01-08 01:59:09 +0800

    feat: add ProductsOverview page

---
+++ src/app/(dashboard)/(management)/products/overview/page.tsx
@@ -0,0 +1,16 @@
+import PageContainer from '@/components/container/page-container'
+import ProductsChart from './_components/products-chart'
+import ProductsSummary from './_components/products-summary'
+
+const ProductsOverview = () => {
+  return (
+    <PageContainer>
+      <div className='grid grid-cols-7 gap-4'>
+        <ProductsChart className='order-2 col-span-7 lg:order-1 lg:col-span-5' />
+        <ProductsSummary className='order-1 col-span-7 lg:order-2 lg:col-span-2' />
+      </div>
+    </PageContainer>
+  )
+}
+
+export default ProductsOverview
