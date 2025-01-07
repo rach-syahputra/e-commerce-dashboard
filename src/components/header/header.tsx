3bdd64fb51df4c21f9da1d502aab497b174325a6
rach-syahputra <rach.syahputra@gmail.com>
2025-01-07 18:49:45 +0800

    feat: add className props for Header component

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,10 +1,18 @@
+import { cn } from '@/lib/utils'
+
 type HeaderProps = {
+  className?: string
   children: React.ReactNode
 }
 
-const Header = ({ children }: HeaderProps) => {
+const Header = ({ className, children }: HeaderProps) => {
   return (
-    <h1 className='text-xl font-bold uppercase text-zinc-800 dark:bg-gradient-to-b dark:from-gray-100 dark:to-gray-300 dark:bg-clip-text dark:text-transparent lg:text-2xl'>
+    <h1
+      className={cn(
+        'text-xl font-bold uppercase text-zinc-800 dark:bg-gradient-to-b dark:from-gray-100 dark:to-gray-300 dark:bg-clip-text dark:text-transparent lg:text-2xl',
+        className
+      )}
+    >
       {children}
     </h1>
   )
