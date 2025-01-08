c637a2a4335a10973cd66cea09f54d2da30fd420
rach-syahputra <rach.syahputra@gmail.com>
2025-01-08 12:11:27 +0800

    feat: add Product Overview page

--- src/app/(dashboard)/(management)/products/record/page.tsx
+++ src/app/(dashboard)/(management)/products/record/page.tsx
@@ -1,10 +1,10 @@
 import PageContainer from '@/components/container/page-container'
-import UserDataTable from './_components/table/user-data-table'
+import ProductDataTable from './_components/table/product-data-table'
 
 export default function UsersRecordPage() {
   return (
     <PageContainer>
-      <UserDataTable />
+      <ProductDataTable />
     </PageContainer>
   )
 }
